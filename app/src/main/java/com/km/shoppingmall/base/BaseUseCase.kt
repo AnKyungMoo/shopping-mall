b70a680999775db7e092c1beb7c34ed3bdc8ad7a
KM <sodp2@naver.com>
2022-01-19 22:58:55 +0900

    feat: #1 usecase의 결과에 추가작업을 진행할 수 있는 함수 생성

--- app/src/main/java/com/km/shoppingmall/base/BaseUseCase.kt
+++ app/src/main/java/com/km/shoppingmall/base/BaseUseCase.kt
@@ -1,16 +1,27 @@
 package com.km.shoppingmall.base
 
-/* TODO: 제네릭을 out 으로 해야될지도..? */
-sealed class Result<T> {
+sealed class Result<out T> {
     data class Success<T>(val value: T) : Result<T>()
     data class Error<T>(val error: Throwable) : Result<T>()
+}
+
+fun <T> Result<T>.processMore(onSuccess: () -> Unit, onError: () -> Unit = {}): Result<T> {
+    when(this) {
+        is Result.Success -> {
+            onSuccess()
+        }
+        is Result.Error -> {
+            onError()
+        }
+    }
+
+    return this
 }
 
 abstract class BaseUseCase {
     protected inline fun <T> execute(block: () -> T): Result<T> = runCatching {
         Result.Success(block())
     }.getOrElse {
-        /* TODO: Success, Error 를 나눠서 관리해야하는 함수가 생기면 그때 printStackTrace 를 이동한다. */
         it.printStackTrace()
         Result.Error(it)
     }
--- app/src/main/java/com/km/shoppingmall/presenter/login/LoginViewModel.kt
+++ app/src/main/java/com/km/shoppingmall/presenter/login/LoginViewModel.kt
@@ -1,7 +1,9 @@
 package com.km.shoppingmall.presenter.login
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.km.shoppingmall.base.processMore
 import com.km.shoppingmall.domain.LoginUseCase
 import com.km.shoppingmall.presenter.login.model.LoginModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,7 +14,14 @@ import javax.inject.Inject
 class LoginViewModel @Inject constructor(private val loginUseCase: LoginUseCase) : ViewModel() {
     fun login(email: String, password: String) {
         viewModelScope.launch {
-            loginUseCase(LoginModel(email, password))
+            loginUseCase(LoginModel(email, password)).processMore(
+                onSuccess = {
+                    Log.d("login", "success")
+                },
+                onError = {
+                    Log.d("login", "fail")
+                }
+            )
         }
     }
 }
